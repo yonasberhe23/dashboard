78080112b8b7ea2738746cc2aa795d070287a646
yonasberhe23 <yonas.berhe@suse.com>
2025-10-03 15:40:42 -0700

    make improvments to harvester tests

--- cypress/e2e/po/components/keyboard-mapping-indicator.po.ts
+++ cypress/e2e/po/components/keyboard-mapping-indicator.po.ts
@@ -1,15 +1,15 @@
 import ComponentPo from '@/cypress/e2e/po/components/component.po';
+import TooltipPo from '@/cypress/e2e/po/components/tooltip.po';
 
 export default class KeyboardMappingIndicatorPo extends ComponentPo {
   constructor() {
     super('[data-testid="code-mirror-keymap"]');
   }
 
-  showTooltip(): Cypress.Chainable {
-    return this.self().trigger('mouseenter');
-  }
-
-  getTooltipContent(): Cypress.Chainable {
-    return cy.get('.v-popper__popper.v-popper--theme-tooltip .v-popper__inner');
+  /**
+   * Get tooltip functionality for this component
+   */
+  tooltip(): TooltipPo {
+    return new TooltipPo(this.self());
   }
 }
