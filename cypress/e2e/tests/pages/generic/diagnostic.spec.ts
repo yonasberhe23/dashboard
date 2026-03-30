c0471ad83e724874428e737fb6a102a2f2c30498
Isabela Guimaraes <isabela.guimaraes@suse.com>
2026-03-30 09:14:30 -0700

    added qase id
    Signed-off-by: Isabela Guimaraes <isabela.guimaraes@suse.com>

--- cypress/e2e/tests/pages/generic/diagnostic.spec.ts
+++ cypress/e2e/tests/pages/generic/diagnostic.spec.ts
@@ -1,5 +1,6 @@
 import DiagnosticsPagePo from '@/cypress/e2e/po/pages/diagnostics.po';
 import * as path from 'path';
+import { qase } from '@/cypress/support/qase';
 
 const downloadsFolder = Cypress.config('downloadsFolder');
 const downloadedFilename = path.join(downloadsFolder, 'rancher-diagnostic-data.json');
@@ -9,7 +10,7 @@ describe('Diagnostics Page', { tags: ['@generic', '@adminUser'] }, () => {
     cy.login();
   });
 
-  it('User should be able to download the diagnostics package JSON', () => {
+  qase(1454, it('User should be able to download the diagnostics package JSON', () => {
     // Ignore the focus-trap error that fires when the modal closes immediately
     // after the download is triggered (known cosmetic side-effect of the dialog)
     // Also the focus-trap error triggered when it can’t find any tabbable node inside its container
@@ -41,7 +42,7 @@ describe('Diagnostics Page', { tags: ['@generic', '@adminUser'] }, () => {
     cy.readFile(downloadedFilename).should('exist').then((file: any) => {
       expect(Object.keys(file).length).to.equal(4);
     });
-  });
+  }));
   afterEach(() => {
     // Keep the downloads directory clean between tests.
     cy.exec(`rm -f "${ downloadedFilename }"`, { failOnNonZeroExit: false });
